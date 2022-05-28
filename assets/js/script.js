490bb7968f323e8a1ba75818a96dd0154461f9d8
Sm4ck <mikeamartinez314@gmail.com>
2022-05-27 21:41:32 -0500

    save tasks to localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,6 +52,7 @@ var createTaskEl = function(taskDataObj) {
   taskDataObj.id = taskIdCounter;
   tasks.push(taskDataObj);
 
+  saveTasks()
   // create task actions (buttons and select) for task
   var taskActionsEl = createTaskActions(taskIdCounter);
   listItemEl.appendChild(taskActionsEl);
@@ -117,6 +118,8 @@ var completeEditTask = function(taskName, taskType, taskId) {
           tasks[i].name = taskName;
           tasks[i].typr = taskType;
       }
+
+    saveTasks()
   };
 
   alert("Task Updated!");
@@ -166,6 +169,8 @@ var taskStatusChangeHandler = function(event) {
         tasks[i].status = statusValue;
     }
   }
+
+  saveTasks()
 };
 
 var editTask = function(taskId) {
@@ -206,7 +211,13 @@ var deleteTask = function(taskId) {
   }
 
   tasks = updatedTaskArr;
+
+  saveTasks()
 };
+
+var saveTasks = function()  {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
 
 // Create a new task
 formEl.addEventListener("submit", taskFormHandler);
